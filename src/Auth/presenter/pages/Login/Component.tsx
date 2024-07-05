12fc890e822f2f2628772c27ae663155428158f0
Jesus Perez <jesusgperez16@gmail.com>
2024-07-05 15:07:07 -0500

    Create the first version of the login page

--- src/Auth/presenter/pages/Login/Component.tsx
+++ src/Auth/presenter/pages/Login/Component.tsx
@@ -1,12 +1,58 @@
 import { useContext } from "react"
 import { LoginProps } from "./models"
 import AuthContext from "../../contexts/Context"
+import { Input, Button } from "../../../../common/presenter/components"
 
 const Component = ({}: LoginProps) => {
-  const { setLoginData } = useContext(AuthContext)
+  const { loginData, setLoginData } = useContext(AuthContext)
+
+  const setValue = (type: string, value: string) => {
+    if (type === 'email') {
+      setLoginData({...loginData, email: value})
+    }
+    else if (type === 'password') {
+      setLoginData({...loginData, password: value})
+    }
+  }
 
   return (
-      <h1>Hello world</h1>
+      <div 
+        className="flex justify-center items-center bg-center bg-cover bg-no-repeat bg-wave h-screen w-screen"
+      >
+        <div
+          className="relative flex flex-col items-center bg-white rounded-lg shadow-2xl space-y-10 border border-gray w-3/4 h-1/3 md:space-y-0 md:h-1/2 lg:w-5/12"
+        >
+          <div
+            className="absolute mx-auto -mt-10 rounded-full bg-blue-100 h-20 w-20 fill-blue-100 md:h-40 md:w-40 md:-mt-20"
+          >
+            <img src="src/assets/logo.svg" alt="" />
+          </div>
+          <div
+            className="flex flex-col justify-center items-center w-full h-full bg-gray space-y-5 px-5 -mt-10 md:-mt-20 md:px-10"
+          >
+              <Input
+                value={loginData.email}
+                placeholder="Email"
+                setValue={ (value) => setValue('email', value)}
+                containerStyles="w-3/4"
+              />
+
+              <Input
+                value={loginData.password}
+                placeholder="Password"
+                setValue={ (value) => setValue('password', value)}
+                containerStyles="w-3/4"
+                type="password"
+              />
+
+              <Button
+                text="Ingresar"
+                onClick={() => {console.log(loginData)}}
+                customStyles="bg-blue-400"
+              />
+          </div>
+        </div>
+      </div>
   )
 }
 
--- src/common/presenter/components/Input/Input.tsx
+++ src/common/presenter/components/Input/Input.tsx
@@ -1,18 +1,21 @@
 import { InputProps } from "./models"
 
-const Input = (props: InputProps) => {
-  const hasError = props.errors ? props.errors.hasError : false
+const Input = ({
+  errors, containerStyles, customStyles, placeholder, value, setValue, type
+}: InputProps) => {
+  const hasError = errors ? errors.hasError : false
 
   return (
-    <div className={`flex flex-col ${props.containerStyles} `}>
+    <div className={`flex flex-col ${containerStyles} `}>
       <input
-        className={`container outline-none rounded-lg border border-black h-9 ${props.customStyles} ${!!props.value ? 'text-sm' : 'text-xs'}`}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChange={(e) => props.setValue(e.target.value)}
+        className={`container outline-none rounded-lg border border-black h-9 text-center ${customStyles} ${!!value ? 'text-sm' : 'text-xs'}`}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type={`${type || 'text'}`}
       />
       <p className={`text-xs text-red-500 ${!hasError ? 'hidden' : 'block'}`}>
-        {props.errors ? props.errors.message : ""}
+        {errors ? errors.message : ""}
       </p>
     </div>
   )
