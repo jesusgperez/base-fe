75a14fb69cdf07884a96e5e936700dfd089bfa62
Jesus Perez <jesusgperez16@gmail.com>
2024-07-09 09:57:46 -0500

    Simplifying the login

--- src/Auth/presenter/pages/Login/Presenter.tsx
+++ src/Auth/presenter/pages/Login/Presenter.tsx
@@ -1,37 +1,17 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import Component from "./Component"
 import { useLogin } from "../../hooks"
-import { useLocalStorage } from "../../../../common/presenter/hooks"
 import AuthContext from "../../contexts/Context"
 import {
   Modal, ModalTypes, DefaultModalState
 } from "../../../../common/presenter/components"
-import { ILoginEntity, ITokenEntity } from "../../../domain/models"
-import { jwtDecode } from "jwt-decode"
-import { useNavigate } from "react-router-dom"
+import { ILoginEntity } from "../../../domain/models"
 
 
 const Presenter = () => {
-  const storage = useLocalStorage()
-  const navigate = useNavigate()
 
   const { mutateAsync: loginUser } = useLogin()
   const { modalState, setModalState } = useContext(AuthContext)
-
-  useEffect(() => {
-    // const token: ITokenEntity = storage.getStorage('token')
-
-    // if (!token.refreshToken) {
-    //   return
-    // }
-
-    // const decodedToken = jwtDecode(token.refreshToken)
-
-    // if(Date.now() < decodedToken.exp! * 1000) {
-    //   navigate('/home')
-    //   return
-    // }
-  }, [])
 
   const onLogin = (loginData: ILoginEntity) => {
     const login = async () => await loginUser(loginData)
