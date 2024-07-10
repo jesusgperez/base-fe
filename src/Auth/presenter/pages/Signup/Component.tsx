18526cbbb696f20bfb14ddc5e7c5c8865a97140c
Jesus Perez <jesusgperez16@gmail.com>
2024-07-10 10:33:26 -0500

    Create the basic layout

--- src/Auth/presenter/pages/Signup/Component.tsx
+++ src/Auth/presenter/pages/Signup/Component.tsx
@@ -20,8 +20,23 @@ const Component = ({ onSignup }: SignupProps) => {
 
   return (
     <AuthBackgroundLayout>
-      <h1>This is the signup</h1>
-      {isValid}
+      <div className="flex flex-col md:flex-row w-full h-full">
+        <form
+          onSubmit={handleSubmit(onSignup)}
+          className="flex justify-center items-center w-full h-full lg:w-1/2"
+        >
+          <div className="flex justify-center items-center rounded-lg shadow-xl w-5/6 h-1/2 bg-white border-2 border-black-700 md:w-3/4">
+
+          </div>
+        </form>
+        <div className="flex justify-center items-center hidden w-1/2 h-full lg:block">
+          <img
+            src="src/assets/logo.svg"
+            alt=""
+            className="hover:animate-spin duration-1000"
+          />
+        </div>
+      </div>
     </AuthBackgroundLayout>
   )
 }
