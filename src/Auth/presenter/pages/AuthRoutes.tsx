12bd8ced7d1f4f7b28a34219651a3210a3299f9f
Jesus Perez <jesusgperez16@gmail.com>
2024-07-12 14:05:54 -0500

    Create page structure

--- src/Auth/presenter/pages/AuthRoutes.tsx
+++ src/Auth/presenter/pages/AuthRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
+import RetrievePassword from "./RetrievePassword";
 import Home from "./Home";
 import AuthProvider from "../contexts/Provider";
 
@@ -12,7 +13,7 @@ const AuthRoutes = () => {
           <Route path='/' element={<Navigate to='/login'/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
-          <Route path='/forgot-password' element={<h1>Retrieve your password</h1>}/>
+          <Route path='/retrieve-password' element={<RetrievePassword />}/>
           <Route path='/change-password' element={<h1>Change your password</h1>}/>
           <Route path='/home' element={<Home />}/>
       </Routes>
