8313fbe4416c167261b633c863443279a726db7f
Jesus Perez <jesusgperez16@gmail.com>
2024-07-12 14:20:12 -0500

    Shorten AuthUrls

--- src/Auth/presenter/pages/AuthRoutes.tsx
+++ src/Auth/presenter/pages/AuthRoutes.tsx
@@ -5,7 +5,7 @@ import Signup from "./Signup";
 import RetrievePassword from "./RetrievePassword";
 import Home from "./Home";
 import AuthProvider from "../contexts/Provider";
-import { AuthUrls } from "./AuthUrls";
+import { AuthUrls as urls } from "./AuthUrls";
 
 
 const AuthRoutes = () => {
@@ -13,11 +13,11 @@ const AuthRoutes = () => {
     <AuthProvider>
       <Routes>
           <Route path='/' element={<Navigate to='/login'/>}/>
-          <Route path={AuthUrls.login} element={<Login/>}/>
-          <Route path={AuthUrls.signup} element={<Signup/>}/>
-          <Route path={AuthUrls.retrievePassword} element={<RetrievePassword />}/>
-          <Route path={AuthUrls.changePassword} element={<h1>Change your password</h1>}/>
-          <Route path={AuthUrls.home} element={<Home />}/>
+          <Route path={urls.login} element={<Login/>}/>
+          <Route path={urls.signup} element={<Signup/>}/>
+          <Route path={urls.retrievePassword} element={<RetrievePassword />}/>
+          <Route path={urls.changePassword} element={<h1>Change your password</h1>}/>
+          <Route path={urls.home} element={<Home />}/>
       </Routes>
     </AuthProvider>
   )
