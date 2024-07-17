26fd4ad602c6775410aebfbad184a658942d934d
Jesus Perez <jesusgperez16@gmail.com>
2024-07-17 10:11:29 -0500

    Create the hooks

---
+++ src/Auth/presenter/hooks/useChange.hook.ts
@@ -0,0 +1,41 @@
+import injections from '../injections'
+import { useContext } from "react"
+import AuthContext from "../contexts/Context"
+import { useNavigate } from "react-router-dom"
+import { useMutation } from '@tanstack/react-query'
+import { IChangeEntity } from '../../domain/models'
+import { IServerError } from '../../../common/domain/models'
+import { AuthUrls } from '../pages/AuthUrls'
+
+
+const useChange = () => {
+  const navigate = useNavigate()
+
+  const {
+    setModalState
+  } = useContext(AuthContext)
+
+  return useMutation<IChangeEntity, Error, IChangeEntity>({
+    mutationFn: (data) => injections.AuthUseCase.Auth.changePassword(data),
+    onSuccess: () => {
+      setModalState({
+        title: "Éxito",
+        content: "La contraseña se ha cambiado correctamente, ahora puede iniciar sesión",
+        open: true
+      })
+
+      navigate(AuthUrls.login)
+    },
+    onError: (e: unknown) => {
+      const error = e as IServerError
+
+      setModalState({
+        title: "Error",
+        content: error.detail,
+        open: true
+      })
+    }
+  })
+}
+
+export { useChange }
