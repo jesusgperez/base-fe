18ff21025aafae516d38992ff163dc20d0c4be5d
Jesus Perez <jesusgperez16@gmail.com>
2024-07-17 09:55:42 -0500

    Adding page structure

---
+++ src/Auth/presenter/pages/ChangePassword/Component.tsx
@@ -0,0 +1,11 @@
+import { ChangePasswordProps } from "./models"
+
+
+const Component = ({}: ChangePasswordProps) => {
+  return (
+    <></>
+  )
+}
+
+
+export default Component
---
+++ src/Auth/presenter/pages/ChangePassword/Presenter.tsx
@@ -0,0 +1,11 @@
+import Component from "./Component"
+
+
+const Presenter = () => {
+  return (
+    <Component />
+  )
+}
+
+
+export default Presenter
